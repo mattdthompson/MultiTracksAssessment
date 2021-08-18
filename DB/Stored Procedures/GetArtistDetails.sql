aecaeb4ed6f9956245dd0d785cc8c49e400ef454
Matt Thompson <mattt@ntiwa.com>
2021-08-18 08:37:27 -0700

    artist Details page use DB information

--- DB/Stored Procedures/GetArtistDetails.sql
+++ DB/Stored Procedures/GetArtistDetails.sql
@@ -8,6 +8,19 @@ BEGIN
 		, art.biography		ArtistBiography
 		, art.imageURL		ArtistImage
 		, art.heroURL		ArtistHero
+		, alb.title			AlbumTitle
+		, alb.year			AlbumYear
+		, alb.imageURL		AlbumImg
+		, s.title			SongTitle
+		, s.bpm				SongBPM
+		, s.timeSignature	SongTimeSignature
+		, s.multitracks		SongMultiTracks
+		, s.customMix		SongCustomMix
+		, s.chart			SongChart
+		, s.rehearsalMix	SongRehearsalMix
+		, s.patches			SongPatches
+		, s.songSpecificPatches	SongSpecificPatches
+		, s.proPresenter	SongProPresenter
 	FROM Artist art
 	LEFT OUTER JOIN Album alb ON alb.artistID = art.artistID
 	LEFT OUTER JOIN Song s ON s.albumID = alb.albumID
--- Web/multitracks.com/multitracks.com/PageToSync/artistDetails.aspx.cs
+++ Web/multitracks.com/multitracks.com/PageToSync/artistDetails.aspx.cs
@@ -1,32 +1,55 @@
 ﻿using DataAccess;
 using System;
 using System.Data;
+using System.Linq;
 
 public partial class PageToSync_artistDetails : MultitracksPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        int artistId;
+        var queryArtistId = Request.QueryString["artistID"];
+        Int32.TryParse(queryArtistId, out artistId);
+        if (artistId == 0) artistId = 1;
         var sql = new SQL();
         try
         {
-            sql.Parameters.Add("@artistId", 1);
-            var data = sql.ExecuteStoredProcedureDS("GetArtistDetails");
-
-            HeroImg.ImageUrl = (string)data.Tables[0].FirstOrNewRow().ItemArray[3];
-            AvatarImg.ImageUrl = (string)data.Tables[0].FirstOrNewRow().ItemArray[2];
-            ArtistBio.InnerText = (string)data.Tables[0].FirstOrNewRow().ItemArray[1];
-            ArtistName.InnerText = (string)data.Tables[0].FirstOrNewRow().ItemArray[0];
-            //assessmentItems.DataSource = data;
-            //assessmentItems.DataBind();
-
-            //publishDB.Visible = false;
-            //items.Visible = true;
+            sql.Parameters.Add("@artistId", artistId);
+            var data = sql.ExecuteStoredProcedureDT("GetArtistDetails");
+            SongDisplay.DataSource = data;
+            SongDisplay.DataBind();
+            data.Select();
+            var distinctAlbums = data.AsEnumerable().Select(x => new
+            {
+                AlbumTitle=x.Field<string>("AlbumTitle"),
+                AlbumImg = x.Field<string>("AlbumImg"),
+                ArtistTitle = x.Field<string>("ArtistTitle"),
+            }).Distinct().ToList();
+            AlbumDisplay.DataSource = distinctAlbums;
+            AlbumDisplay.DataBind();
+            HeroImg.ImageUrl = (string)data.Rows[0].ItemArray[3];
+            AvatarImg.ImageUrl = (string)data.Rows[0].ItemArray[2];
+            ArtistBio.InnerText = (string)data.Rows[0].ItemArray[1];
+            ArtistName.InnerText = (string)data.Rows[0].ItemArray[0];
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex);
-            //publishDB.Visible = true;
-            //items.Visible = false;
+        }
+    }
+
+    protected string TimeSignature(string timeSignature)
+    {
+        switch (timeSignature)
+        {
+            case "3":
+                return "4/4";
+            case "13":
+                return "6/8";
+            case "18":
+                return "12/8";
+            default:
+                return "4/4";
         }
     }
 }
